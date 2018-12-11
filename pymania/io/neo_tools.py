3dbda7633ec00878a7878a347170ff15700c3b12
Manoj Chandrasekaran <manoj071289@gmail.com>
2018-12-11 17:40:44 -0500

    Added functions to create and update connections in Neo4j

--- pymania/io/neo_tools.py
+++ pymania/io/neo_tools.py
@@ -53,4 +53,65 @@ def getdata_pair_subs(st,subs):
     out.extend(graph.run(query).data())
     return out
 
+
 #################### writing to neo4j database #######################
+def write_connection(roi1, roi2, relation_type, attributes, run=True):
+    """Write a connection to Neo4j database
+
+    :param roi1: Source ROI
+    :type roi1: str
+    :param roi2: Destination ROI
+    :type roi2: str
+    :param relation_type: Type of relationship to write
+    :type relation_type: str
+    :param attributes: Dictionary of attributes of the link
+    :type attributes: dict
+    :param run: Whether to run the query or return the query
+    :type run: bool
+    :return: The query generated if run is False
+    """
+    match = f"MATCH (n:ROI{{name:'{roi1}'}}), (m:ROI{{name:'{roi2}'}}) "
+    attribs = f"CREATE (n)-[:{relation_type} {{"
+    for key in attributes:
+        if type(attributes[key]) == str:
+            attribs += str(key) + ":'" + str(attributes[key]) + "', "
+        else:
+            attribs += str(key) + ':' + str(attributes[key]) + ', '
+    query = match + attribs[:-2] + '}]->(m)'
+    if run:
+        graph.run(query)
+    else:
+        return query
+
+
+def update_connection(roi1, roi2, relation_type, subject, attributes, run=True):
+    """Update a connection in Neo4j database
+
+    :param roi1: Source ROI
+    :type roi1: str
+    :param roi2: Destination ROI
+    :type roi2: str
+    :param relation_type: Type of relationship to update
+    :type relation_type str
+    :param subject: Subject ID
+    :type subject: int or str
+    :param attributes: Dictionary of attributes to update
+    :type attributes: dict
+    :param run: Whether to run the query or return the query
+    :type run: dict
+    :return: Query generated if run is False
+    """
+    if type(subject) != str:
+        subject = str(subject)
+    match = f"MATCH (n:ROI{{name:'{roi1}'}})-[r:{relation_type}]->(m:ROI{{name:'{roi2}'}}) WHERE r.SUBJECT={subject}"
+    attribs = ' SET '
+    for key in attributes:
+        if type(attributes[key]) == str:
+            attribs += 'r.' + str(key) + "='" + str(attributes[key]) + "', "
+        else:
+            attribs += 'r.' + str(key) + '=' + str(attributes[key]) + ', '
+    query = match + attribs[:-2]
+    if run:
+        graph.run(query)
+    else:
+        return query
